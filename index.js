641cf59180f0c54c7456e3b8d21dec9ddb8757ed
Vladimir <vladimir-nikolic@hotmail.com>
2024-03-06 19:12:12 +0100

    test 1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 require('dotenv').config();
+var os = require('node:os')
 var express = require('express');
 var app = express();
 
@@ -24,7 +25,22 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req,res) => {
+    res.json({
+      ipaddress: req.socket.remoteAddress,
+      language: req.headers["accept-language"],
+      software: req.headers["user-agent"],
+    });
+});
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+/*  req.socket.remoteAddress
+      //req.headers['host'],
+      req.headers['x-forwarded-for'],
+      req.headers["accept-language"],
+      req.headers['user-agent'],
+      */
